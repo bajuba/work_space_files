8bed7a0772e2775e721b10d8e637d6da789fb0f4
zedboson <walters.ralph@outlook.com>
2018-08-21 11:51:43 -0500

    Added phpMyAdmin install script

--- anaconda_install.sh
+++ anaconda_install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cd ~/Downloads
 wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
 bash Anaconda3-5.2.0-Linux-x86_64.sh
---
+++ phpMyAdmin_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yum install php-mbstring -y
+systemctl restart httpd
+systemctl restart php-fpm
+cd /var/www/html
+wget https://files.phpmyadmin.net/phpMyAdmin/4.8.2/phpMyAdmin-4.8.2-all-languages.tar.gz
+mkdir phpMyAdmin
+chmod -R 755 phpMyAdmin
+tar -xvzf phpMyAdmin-4.8.2-all-languages.tar.gz -C phpMyAdmin --strip-components 1
+rm phpMyAdmin-4.8.2-all-languages.tar.gz
+systemctl start mariadb
+google-chrome --no-sandbox http://localhost/phpMyAdmin
