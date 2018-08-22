cdd85196139425226cb3d095611a0d070f50cf3c
Ralph Walters <ralphwalters@rwaltmacbookpro.local>
2018-08-22 14:10:56 -0500

    Changed phpMyAdmin install script for new version

--- phpMyAdmin_install.sh
+++ phpMyAdmin_install.sh
@@ -3,7 +3,7 @@ yum install php-mbstring -y
 systemctl restart httpd
 systemctl restart php-fpm
 cd /var/www/html
-wget https://files.phpmyadmin.net/phpMyAdmin/4.8.2/phpMyAdmin-4.8.2-all-languages.tar.gz
+wget https://files.phpmyadmin.net/phpMyAdmin/4.8.3/phpMyAdmin-4.8.3-all-languages.tar.gz
 mkdir phpMyAdmin
 chmod -R 755 phpMyAdmin
 tar -xvzf phpMyAdmin-4.8.2-all-languages.tar.gz -C phpMyAdmin --strip-components 1
